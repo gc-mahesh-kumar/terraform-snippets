1f80493c84562880637f4672a6710336360b63a9
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-07-05 04:26:22 +0000

    added gce

--- modules/gce/output.tf
+++ modules/gce/output.tf
@@ -1,19 +1,19 @@
-output "vm_ip" {
-  value = google_compute_instance.default.network_interfaces[0].network_ip
-}
+# output "vm_ip" {
+#   value = google_compute_instance.default.network_interfaces[0].network_ip
+# }
 
-output "vm_name" {
-  value = google_compute_instance.default.name
-}
+# output "vm_name" {
+#   value = google_compute_instance.default.name
+# }
 
-output "vm_zone" {
-  value = google_compute_instance.default.zone
-}
+# output "vm_zone" {
+#   value = google_compute_instance.default.zone
+# }
 
-output "vm_network" {
-  value = google_compute_instance.default.network
-}
+# output "vm_network" {
+#   value = google_compute_instance.default.network
+# }
 
-output "vm_machine_type" {
-  value = google_compute_instance.default.machine_type
-}
+# output "vm_machine_type" {
+#   value = google_compute_instance.default.machine_type
+# }
