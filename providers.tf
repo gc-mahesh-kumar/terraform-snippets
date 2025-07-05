103e9d3441296e8455efe290cd9a5560d5d624cf
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-07-05 01:32:31 +0000

    added backend section

--- providers.tf
+++ providers.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.41.0"
     }
+  }
+
+  #Manually create this bucket  
+  backend "gcs" {
+    bucket = "tf-state-pca-concepts"
+    prefix = "terraform/state/dev"
   }
 }
 
