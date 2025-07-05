f2de15c472d2df2a54023d285396336443f919e3
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-07-05 01:20:32 +0000

    added providers file

--- gcs.tf
+++ gcs.tf
@@ -1,7 +1,3 @@
-provider "google" {
-  project = "cloud-custom-demos"
-}
-
 resource "google_storage_bucket" "bucket_01" {
   name          = "gcs-concepts-tf-demo-01"
   location      = "us-central1"
--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   required_providers {
-#     google = {
-#       source = "hashicorp/google"
-#       version = "6.41.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.41.0"
+    }
+  }
+}
 
-# provider "google" {
-#   project = "cloud-custom-demos"
-# }
+provider "google" {
+  project = "cloud-custom-demos"
+}
