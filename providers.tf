a534245975bb89b2572fbf5f97e61b2460eb37a6
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-07-05 01:14:38 +0000

    removed folder

---
+++ providers.tf
@@ -0,0 +1,12 @@
+# terraform {
+#   required_providers {
+#     google = {
+#       source = "hashicorp/google"
+#       version = "6.41.0"
+#     }
+#   }
+# }
+
+# provider "google" {
+#   project = "cloud-custom-demos"
+# }
--- simplest/gcs.tf
+++
@@ -1,9 +0,0 @@
-provider "google" {
-  project = "cloud-custom-demos"
-}
-
-resource "google_storage_bucket" "bucket_02" {
-  name = "gcs-concepts-tf-demo-02"
-  location = "us-central1"
-  storage_class = "NEARLINE"
-}
